df3b02b7cffcc514906cdd3295d394e51aeaba0d
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 14:34:38 +0000

    Automatically backed up by Learn

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,10 +1,9 @@
 # Implement your object-oriented solution here!
 class LargestPalindromeProduct 
   def initialize 
-    @anwer = 906609 
   end 
   
   def answer 
-    @answer 
+    906609
   end
 end 
